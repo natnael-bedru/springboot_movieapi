db4c54563f8ff3cb571adcaeb81e42ae31348dc1
natnael-bedru <natnaelbedru@gmail.com>
2023-07-18 15:01:31 +0300

    Changes Added changes      [+] .env file in resources      [+] Movie               [+] Class               [+] Controller                    [+] getAllMovies                    [+] getSingleMovie [ObjectId id & String imdbId]               [+] Service                    [+] allMovies                    [+] singleMovie [ObjectId id & String imdbId]               [+] Repository (interface)                    [*] findAll                    [+] findMovieByImdbId [String imdb]
              [+] Review
                  [+] Class
    Dependency changes
             [+] Spring Dotenv v2.5.4 (https://mvnrepository.com/artifact/me.paulschwarz/spring-dotenv/2.5.4)
               - issues in the v3.0.0 & v4.0.0 (https://github.com/paulschwarz/spring-dotenv)

--- src/main/java/com/nati/movies/MovieController.java
+++ src/main/java/com/nati/movies/MovieController.java
@@ -1,13 +1,16 @@
 package com.nati.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -32,5 +35,18 @@ public class MovieController {
              returning a ResponseEntity of type list movie
         */
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+/*    Object Id
+      @GetMapping("/{id}")
+      public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+          // this uses collection ids of the collection entities ..
+          return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+     }
+ */
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        // this uses collection ids of the collection entities ..
+        // if it doesn't exist returns 'null'
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 }
--- src/main/java/com/nati/movies/MovieService.java
+++ src/main/java/com/nati/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.nati.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -17,5 +19,16 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+    /*
+    Object Id
+    public Optional<Movie> singleMovie(ObjectId id) {
+        // The Optional tag allows if there isn't any movie by that id
+        // this uses collection ids of the collection entities ..
+        return movieRepository.findById(id);
+    }
+    */
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
